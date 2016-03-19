bac96e708264ded02dad1ad0be2a69e1390ea981
pkbest42 <Best@pairojs-MacBook-Pro.local>
2016-03-19 20:03:35 +0700

    deploy third time

--- app/models/note.rb
+++ app/models/note.rb
@@ -23,7 +23,6 @@ class Note < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   has_many :note_images
-  accepts_nested_attributes_for :images
   searchkick
 
   validates :title, presence: true
